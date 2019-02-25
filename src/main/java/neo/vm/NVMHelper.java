40f0965cb32cf14433ed104a82841a96eaf8e654
Li-Tian <tianli@neo.org>
2019-02-25 16:00:12 +0800

    NVMHelper tested

--- src/main/java/neo/vm/NVMHelper.java
+++ src/main/java/neo/vm/NVMHelper.java
@@ -1,45 +1,125 @@
 package neo.vm;
 
-import neo.log.tr.TR;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+import neo.log.notr.TR;
 
 /**
- * 这个类是从 Neo.VM.Helper 移植过来。
- * 因为 C# 版的代码中这个类与 Neo.dll 中的另一个类同名。
- * 所以移植到 Java 版时，这个类被重命名为 NVMHelper
+ * 这个类是从 Neo.VM.Helper 移植过来。 因为 C# 版的代码中这个类与 Neo.dll 中的另一个类同名。 所以移植到 Java 版时，这个类被重命名为 NVMHelper
  */
 class NVMHelper {
-    public static byte[] ReadVarBytes() {
-        TR.enter();
 
-        return TR.exit(null);
+    /**
+     * 从指定输入流中读取可变长的字节数组
+     *
+     * @param reader 输入流
+     * @return 字节数组
+     * @throws IOException 读取失败
+     */
+    public static byte[] readVarBytes(InputStream reader) throws IOException {
+        TR.enter();
+        return TR.exit(readVarBytes(reader, 0x10000000));
     }
-// TODO
-//    public static byte[] ReadVarBytes(BinaryReader reader, int max = 0x10000000)
-//    {
-//        return reader.SafeReadBytes((int)reader.ReadVarInt((ulong)max));
-//    }
-//
-//    public static ulong ReadVarInt(this BinaryReader reader, ulong max = ulong.MaxValue)
-//    {
-//        byte fb = reader.ReadByte();
-//        ulong value;
-//        if (fb == 0xFD)
-//            value = reader.ReadUInt16();
-//        else if (fb == 0xFE)
-//            value = reader.ReadUInt32();
-//        else if (fb == 0xFF)
-//            value = reader.ReadUInt64();
-//        else
-//            value = fb;
-//        if (value > max) throw new FormatException();
-//        return value;
-//    }
-//
-//    public static byte[] SafeReadBytes(this BinaryReader reader, int count)
-//    {
-//        byte[] data = reader.ReadBytes(count);
-//        if (data.Length < count)
-//            throw new FormatException();
-//        return data;
-//    }
+
+    /**
+     * 从指定输入流中读取可变长的字节数组
+     *
+     * @param reader 输入流
+     * @param max    指定最大字节数
+     * @return 字节数组
+     * @throws IOException 读取失败
+     */
+    public static byte[] readVarBytes(InputStream reader, int max) throws IOException {
+        TR.enter();
+        int length = (int) readVarInt(reader, max);
+        byte[] result = safeReadBytes(reader, length);
+        return TR.exit(result);
+    }
+
+    /**
+     * 从字节流中读取变长整数
+     *
+     * @param reader 字节流
+     * @return 读取的整数。由于java没有ULONG，能读取的最大整数与C#版相比，要小大约一半。
+     * @throws IOException 读取失败。数据格式不正确。
+     */
+    public static long readVarInt(InputStream reader) throws IOException {
+        TR.enter();
+        return TR.exit(readVarInt(reader, Long.MAX_VALUE));
+    }
+
+    /**
+     * 从字节流中读取变长整数
+     *
+     * @param reader 字节流
+     * @param max    指定的最大值，如果读取的值超过此值则认定为数据格式错误，抛出IOException
+     * @return 读取的整数。由于java没有ULONG，能读取的最大整数与C#版相比，要小大约一半。
+     * @throws IOException 读取失败。数据格式不正确。
+     */
+    public static long readVarInt(InputStream reader, long max) throws IOException {
+        TR.enter();
+        long fb = reader.read();
+        if (fb < 0 || fb > 255) {
+            TR.exit();
+            throw new IOException("Wrong format for VarInt");
+        }
+        long value;
+        if (fb == 0xFDL) {
+            byte[] temp = new byte[2];
+            reader.read(temp);
+            temp = reverse(temp);
+            value = ByteBuffer.wrap(temp).getShort() & 0xFFFFL;
+        } else if (fb == 0xFEL) {
+            byte[] temp = new byte[4];
+            reader.read(temp);
+            temp = reverse(temp);
+            value = ByteBuffer.wrap(temp).getInt() & 0xFFFFFFFFL;
+        } else if (fb == 0xFF) {
+            byte[] temp = new byte[8];
+            reader.read(temp);
+            temp = reverse(temp);
+            value = ByteBuffer.wrap(temp).getLong();
+            if (value < 0) {
+                TR.exit();
+                throw new IOException("VarInt out of range of long in java");
+            }
+        } else {
+            value = fb;
+        }
+        if (value > max) {
+            throw new IOException("VarInt out of specified range : " + value);
+        }
+        return TR.exit(value);
+    }
+
+    private static byte[] reverse(byte[] data) {
+        byte[] result = new byte[data.length];
+        for (int i = 0; i < data.length; i++) {
+            result[i] = data[data.length - 1 - i];
+        }
+        return result;
+    }
+
+    /**
+     * 安全读取字节数组
+     *
+     * @param reader 输入数据流
+     * @param count  指定读取字节数组的长度
+     * @return 成功读取的字节数组
+     * @throws IOException 任何未能成功读取到指定字节长度的数据的原因。
+     */
+    public static byte[] safeReadBytes(InputStream reader, int count) throws IOException {
+        TR.enter();
+        byte[] data = new byte[count];
+        int countRead = reader.read(data);
+        if (countRead < count) {
+            TR.exit();
+            throw new IOException(String.format(
+                    "try to read %d bytes, actual returned %d bytes", count, countRead));
+        }
+        TR.exit();
+        return data;
+    }
 }
--- src/test/java/neo/vm/NVMHelperTest.java
+++ src/test/java/neo/vm/NVMHelperTest.java
@@ -1,25 +1,37 @@
 package neo.vm;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
 public class NVMHelperTest {
 
     @Test
     public void testReadVarBytes() {
-        // TODO
-        NVMHelper.ReadVarBytes();
+        byte[] sample = {5, 'a', 'b', 'c', 'd', 'e'};
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(sample);
+        try {
+            byte[] data = NVMHelper.readVarBytes(byteArrayInputStream);
+            byte[] expected = new byte[]{'a', 'b', 'c', 'd', 'e'};
+            Assert.assertArrayEquals(expected, data);
+        } catch (IOException e) {
+            Assert.fail();
+        }
     }
 
     @Test
     public void testReadVarInt() {
-        // TODO
-
-    }
-
-    @Test
-    public void testSafeReadBytes() {
-        // TODO
-
+        byte[] sample = {(byte) 0xFE, 1, 2, 3, 4};
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(sample);
+        long value = 0;
+        try {
+            value = NVMHelper.readVarInt(byteArrayInputStream);
+        } catch (IOException e) {
+            Assert.fail();
+        }
+        Assert.assertEquals(67305985, value);
     }
 
 }
